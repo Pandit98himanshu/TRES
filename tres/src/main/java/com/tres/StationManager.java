7e4913be959332dc1df044dd1320004d561c9e62
Royal Kumar <royalsdik1998@gmail.com>
2021-12-31 12:00:51 +0530

    added StationManager

--- tres/src/main/java/com/tres/StationManager.java
+++ tres/src/main/java/com/tres/StationManager.java
@@ -1,5 +1,23 @@
 package com.tres;
 
+import java.util.Map;
+
 public class StationManager {
+	Map<String,Station> map ;
+
+	public StationManager(Map<String, Station> map) {
+		super();
+		this.map = map;
+	}
+
+	public Map<String, Station> getMap() {
+		return map;
+	}
+
+	public void setMap(Map<String, Station> map) {
+		this.map = map;
+	}
+	
+	
 
 }
