de4dedf2a4825f17cdde4389384436db8872967f
Himanshu Shekhar <pandit98himanshu@gmail.com>
2022-01-11 01:19:26 +0530

    added test case in test file

--- tres/src/test/java/com/tres/AppTest.java
+++ tres/src/test/java/com/tres/AppTest.java
@@ -1,38 +1,164 @@
 package com.tres;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.Assert.assertEquals;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+import org.junit.Test;
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+public class AppTest {
+	
+	Map<String, List<Train>> trains1 = new HashMap<>();
+	List<Train> list1 = new ArrayList<>();
+	Map<String, Halt> halts1 = new HashMap<>();
+	Halt halt1;
+	Map<String, Station> map1 = new HashMap<String, Station>();
+	LocalDateTime arr1, dep1;
+	int duration;
+	StationManager stnMgr1;
+	
+	String[] res;
+	
+	public AppTest() {
+		
+		
+		arr1 = LocalDateTime.parse("2021-12-31T16:03:10.446042");
+		dep1 = LocalDateTime.parse("2021-12-31T16:03:10.446042");
+		duration = 40;
+		halt1 = new Halt(0, arr1, dep1, duration);
+		halts1.put("HWH", halt1);
+		
+		arr1 = LocalDateTime.parse("2021-12-31T16:43:10.446042");
+		dep1 = LocalDateTime.parse("2021-12-31T16:45:10.446042");
+		duration = 65;
+		halt1 = new Halt(1, arr1, dep1, duration);
+		halts1.put("BDC", halt1);
+		
+		arr1 = LocalDateTime.parse("2021-12-31T17:07:10.446042");
+		dep1 = LocalDateTime.parse("2021-12-31T17:10:10.446042");
+		duration = 95;
+		halt1 = new Halt(2, arr1, dep1, duration);
+		halts1.put("MLDT", halt1);
+		
+		
+		list1.add(new Train(13465, "INTERCITY EXP", halts1));
+		trains1.put("FRIDAY", list1);
+		
+		map1.put("HWH", new Station("HWH", "Howrah Jn", trains1));
+		map1.put("BDC", new Station("BDC", "BANDEL JN", trains1));
+		map1.put("MLDT", new Station("MLDT", "Malda Town", trains1));
+		
+		
+		stnMgr1 = new StationManager(map1);
+		
+		
+		Map<String, List<Train>> trains2 = new HashMap<>();
+		List<Train> list2 = new ArrayList<>();
+		Map<String, Halt> halts2 = new HashMap<>();
+		
+		Map<String, Station> map2 = new HashMap<String, Station>();
+		LocalDateTime arr2, dep2;
+	
+		arr2 = LocalDateTime.parse("2022-02-03T06:35:10");
+		dep2 = LocalDateTime.parse("2022-02-03T06:37:10");
+		duration = 35;
+		halt1 = new Halt(0, arr2, dep2, duration);
+		halts2.put("LTT", halt1);
+	
+		arr2 = LocalDateTime.parse("2022-02-03T07:00:00");
+		dep2 = LocalDateTime.parse("2022-02-03T07:02:00");
+		duration = 20;
+		halt1 = new Halt(1, arr2, dep2, duration);
+		halts2.put("JL", halt1);
+	
+		arr2 = LocalDateTime.parse("2022-02-03T11:35:10");
+		dep2 = LocalDateTime.parse("2022-02-03T11:37:10");
+		duration = 50;
+		halt1 = new Halt(2, arr2, dep2, duration);
+		halts2.put("TLV", halt1);
+	
+		arr2 = LocalDateTime.parse("2022-02-03T21:00:00");
+		dep2 = LocalDateTime.parse("2022-02-03T21:02:00");
+		duration = 130;
+		halt1 = new Halt(3, arr2, dep2, duration);
+		halts2.put("ET", halt1);
+	
+		list2.add(new Train(15017, "KASHI EXP", halts2));
+		trains2.put("THURSDAY", list2);
+	
+		Station st1 = new Station("LTT", "LOKMANYATILAK", trains2);
+		Station st2 = new Station("JL", "THANE", trains2);
+		map2.put("LTT", st1);
+		map2.put("JL", st2);
+		map2.put("TLV", new Station("TLV", "TALVADYA", trains2));
+		map2.put("ET", new Station("ET", "ITARSI JN", trains2));
+	
+		stnMgr1 = new StationManager(map2);
+		
+		
+		
+		Map<String, Halt> halts3 = new HashMap<>();
+		LocalDateTime arr3, dep3;
+	
+		arr3 = LocalDateTime.parse("2022-02-03T11:35:10");
+		dep3 = LocalDateTime.parse("2022-02-03T11:37:10");
+		duration = 35;
+		halt1 = new Halt(0, arr3, dep3, duration);
+		halts3.put("LTT", halt1);
+	
+		arr3 = LocalDateTime.parse("2022-02-03T12:00:00");
+		dep3 = LocalDateTime.parse("2022-02-03T12:02:00");
+		duration = 20;
+		halt1 = new Halt(1, arr3, dep3, duration);
+		halts3.put("JL", halt1);
+	
+		arr3 = LocalDateTime.parse("2022-02-03T16:35:10");
+		dep3 = LocalDateTime.parse("2022-02-03T16:37:10");
+		duration = 50;
+		halt1 = new Halt(2, arr3, dep3, duration);
+		halts3.put("TLV", halt1);
+	
+		arr3 = LocalDateTime.parse("2022-02-03T21:00:00");
+		dep3 = LocalDateTime.parse("2022-02-03T21:02:00");
+		duration = 130;
+		halt1 = new Halt(3, arr3, dep3, duration);
+		halts3.put("ET", halt1);
+	
+		Train tr = new Train(13578, "ROYAL EXP", halts3);
+		List<Train> list_New = stnMgr1.getMap().get("LTT").getTrainsOnDay().get("THURSDAY");
+		list_New.add(tr);
+		
+		list_New = stnMgr1.getMap().get("JL").getTrainsOnDay().get("THURSDAY");
+		list_New.add(tr);
+		
+		list_New = stnMgr1.getMap().get("TLV").getTrainsOnDay().get("THURSDAY");
+		list_New.add(tr);
+		
+		list_New = stnMgr1.getMap().get("ET").getTrainsOnDay().get("THURSDAY");
+		list_New.add(tr);
+	}
+	
+	@Test
+	public void testApp() {
+		String src = "LTT";		// LOKMANYATILAK
+		String dest = "TLV";	// TALVADYA
+		String date = "2022-01-13";	// THURSDAY
+		
+		EnquiryManager enqMgr = new EnquiryManager();
+		Map<Train, LocalDateTime> results = enqMgr.searchTrains(src, dest, date, stnMgr1);
+		
+		for (Map.Entry<Train, LocalDateTime> entry : results.entrySet()) {
+	        System.out.println(entry.getKey().getId() + " | " + entry.getKey().getName() + " | " + entry.getValue());
+		}
+		
+		// for above src, dest & date, we'll get below output
+		res = new String[]{"15017 | KASHI EXP | 2022-02-03T06:52:10",
+						"13578 | ROYAL EXP | 2022-02-03T11:52:10"};
+		
+		assertEquals(null, 0, 0);
+	}
 }
